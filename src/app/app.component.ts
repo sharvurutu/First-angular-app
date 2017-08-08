c230beed179ac6905febb15f526236e9e210e3d2
Rutuja <rutu956177@gmail.com>
2017-08-08 12:49:37 +0530

    Menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+export class Menu {
+  id: number;
+  name: string;
+}
+
+const MENUS: Menu[] = [
+  { id: 1, name: 'Italian' },
+  { id: 2, name: 'Indian' },
+  { id: 3, name: 'chinese' },
+];
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>Menu List</h2>
+    <ul class="menus">
+      <li *ngFor="let menu of menus"
+        [class.selected]="menu === selectedMenu"
+        (click)="onSelect(menu)">
+        <span class="badge">{{menu.id}}</span> {{menu.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedMenu">
+      <h2>{{selectedMenu.name}} details!</h2>
+      <div><label>id: </label>{{selectedMenu.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedMenu.name" placeholder="name"/>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Angular App';
+  menus = MENUS;
+  selectedMenu: Menu;
+
+  onSelect(menu: Menu): void {
+    this.selectedMenu = menu;
+  }
 }
